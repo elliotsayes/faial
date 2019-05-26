065366509223a03a5110670b169680c9d44d4d66
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2019-05-25 21:17:30 -0400

    Rewriting projection.

--- src/main.ml
+++ src/main.ml
@@ -1,14 +1,26 @@
 open Proto
 open Sexplib
-
-module StringOT = struct
-  type t = string
-  let compare = Pervasives.compare
-end
-
-module StringSet = Set.Make(StringOT)
+open Common
 
 type list_owned = access timed owned list
+
+let group_multiset l =
+  let groups = Hashtbl.create 0 in
+  let rec iter l =
+    match l with
+    | (x, a)::l -> begin
+      let elems =
+        match Hashtbl.find_opt groups x with
+        | Some elems -> elems
+        | None -> []
+      in
+      Hashtbl.replace groups x (a::elems);
+      iter l
+      end
+    | [] -> ()
+  in
+  iter l;
+  groups
 
 let group_steps (l: (string *access timed owned) list) : (string, list_owned) Hashtbl.t =
   let groups = Hashtbl.create 0 in
@@ -31,7 +43,7 @@ let group_steps (l: (string *access timed owned) list) : (string, list_owned) Ha
 let as_set (l:string list) : StringSet.t =
   List.fold_right StringSet.add l StringSet.empty
 
-let extract_free_names (h: (string, list_owned) Hashtbl.t)
+let extract_free_names h
   : (string, StringSet.t) Hashtbl.t =
   let result = Hashtbl.create 0 in
   Hashtbl.iter (fun x elems ->
@@ -68,15 +80,15 @@ let check (p:proto) =
   let c = Loops.get_constraints p in
   (* 2. Flatten outer loops *)
   let steps = Loops.remove_loops p in
+  (* 3. Get the local variables defined in steps *)
+  let locals = List.fold_right (fun (_, t) fns -> Freenames.free_names_timed t fns) steps [] in
+  let locals = locals |> StringSet.of_list in
   (* 3. Make the owner of each access explicit *)
-  let steps = Spmd2binary.split steps in
+  let steps = Spmd2binary.project_stream locals steps in
   (* 4. Perform a constant-fold optimization, to reduce the problem space *)
   let owned_steps = Constfold.stream_opt steps in
-  (* 5. Convert a flat owned-accs-program into a Hoare Logic program,
-     yielding a formula *)
-  (* 6. Flatten out our theory into First-Order Logic with numbers. *)
-  (* 7. Generate Z3 *)
-  Constfold.b_opt c, owned_steps
+  let c1, c2 = Spmd2binary.project_condition locals c in
+  c1, c2, owned_steps
 
 let join sep elems =
   let on_elem accum x =
@@ -89,7 +101,7 @@ let join sep elems =
 let () =
   let s : Sexp.t = Sexp.input_sexp stdin in
     try
-      let (c, elems) = Parse.parse_proto.run s |> check in
+      let (c1, c2, elems) = Parse.parse_proto.run s |> check in
       let groups = group_steps elems in
       let fns = extract_free_names groups in
       Hashtbl.iter (fun x elems ->
@@ -100,8 +112,10 @@ let () =
         let x_fns = Hashtbl.find fns x in
         join ", " (StringSet.elements x_fns) |> print_endline;
         (* Print the pre-conditions of each location *)
-        print_string "Pre: ";
-        restrict_bexp c x_fns |> Serialize.b_ser |> Sexp.to_string_hum |> print_endline;
+        print_string "Pre (tid1): ";
+        restrict_bexp c1 x_fns |> Serialize.b_ser |> Sexp.to_string_hum |> print_endline;
+        print_string "Pre (tid2): ";
+        restrict_bexp c2 x_fns |> Serialize.b_ser |> Sexp.to_string_hum |> print_endline;
         (* Print the various accesses *)
         List.iter (fun o ->
           Serialize.o_ser o |> Sexp.to_string_hum |> print_endline
--- src/spmd2binary.ml
+++ src/spmd2binary.ml
@@ -1,16 +1,24 @@
 open Proto
+open Common
 
-let tid1 = "$TID1"
-let tid2 = "$TID2"
+let tid1 = "1:"
+let tid2 = "2:"
 
+let project prefix x = prefix ^ x
 
-let as_binary (t:access timed) : (access timed) owned * (access timed) owned =
+let do_project locals prefix =
+  fun x ->
+    if StringSet.mem x locals
+    then Some (Var (project prefix x))
+    else None
+
+let project_access locals (t:access timed) : (access timed) owned * (access timed) owned =
   match t with
   | {timed_phase=n; timed_data=a} ->
     let mk ti =
-      let si = Subst.replace_by (tid, Var ti) in
+      let si = do_project locals ti in
       {
-        owned_tid = ti;
+        owned_tid = project ti tid;
         owned_data = {
           timed_phase=Subst.n_subst si n;
           timed_data={
@@ -23,10 +31,14 @@ let as_binary (t:access timed) : (access timed) owned * (access timed) owned =
     in
     mk tid1, mk tid2
 
-let split (l:(string * access timed) list)
+let project_condition locals (b:bexp) =
+  let do_subst ti = Subst.b_subst (do_project locals ti) in
+  do_subst tid1 b, do_subst tid2 b
+
+let project_stream locals (l:(string * access timed) list)
     : (string * (access timed) owned) list =
   let on_elem (x, t) =
-    let a1, a2 = as_binary t in
+    let a1, a2 = project_access locals t in
     [(x, a1); (x, a2)]
   in
   List.flatten (List.map on_elem l)
