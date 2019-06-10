00de0bcf2ec72a34b09b958502c0976ea3cb91ce
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2019-06-10 18:41:48 -0400

    Install dependencies.

---
+++ configure.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if which opam > /dev/null; then
+  opam install \
+	ocamlbuild=0.14.0 \
+	menhir=20181113 \
+	sexplib=v0.12.0 \
+	ounit=2.0.8 \
+        cmdliner=1.0.3
+else
+  >&2 echo "ERROR: Install opam first!"
+  exit 1
+fi
