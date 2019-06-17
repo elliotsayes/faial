ae78779172ecf713a20d489c796e20f900b18e30
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2019-06-17 10:04:32 -0400

    Fixed test.

--- src/test_loops.ml
+++ src/test_loops.ml
@@ -1,10 +1,12 @@
 open OUnit2
 open Loops
+open Proto
 
 let tests = "loops" >::: [
   "generate_test_names" >:: (fun _ ->
-      assert_equal (generate_fresh_name "foo" ["foo"; "bar"]) "foo1"
+      assert_equal (generate_fresh_name (var_make "foo") (VarSet.of_list [(var_make "foo"); (var_make "bar")])) (var_make "foo1")
   )
 ]
 
 let _ = run_test_tt_main tests
+
