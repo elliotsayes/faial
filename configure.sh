831d8a00f3e205ffd75ab60a89d9965210add34c
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2019-10-28 12:41:54 -0400

    Added an explicit dependency on Z3.

--- configure.sh
+++ configure.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 if which opam > /dev/null; then
-  opam install \
-	ocamlbuild=0.14.0 \
-	menhir=20181113 \
-	sexplib=v0.12.0 \
-	ounit=2.0.8 \
+    opam install \
+        ocamlbuild=0.14.0 \
+        menhir=20181113 \
+        sexplib=v0.12.0 \
+        ounit=2.0.8 \
         cmdliner=1.0.3 \
         ANSITerminal=0.8.1 \
-        yojson=1.7.0
+        yojson=1.7.0 \
+        z3
 else
-  >&2 echo "ERROR: Install opam first!"
-  exit 1
+    >&2 echo "ERROR: Install opam first!"
+    exit 1
 fi
