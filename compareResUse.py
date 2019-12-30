541ce12a37ad670aa9bb02b76cf0aa54bc42bbb4
dennisliew11 <dennisliew11@hotmail.com>
2019-12-30 17:01:18 -0500

    Scripts now uses single thread GVtester. Faialtester outputs csv file.

--- compareResUse.py
+++ compareResUse.py
@@ -12,16 +12,17 @@ def main(arg):
     parser = argparse.ArgumentParser(description='Record Time and Memory usage.')
     parser.add_argument('--input-file',type=str, required=True, \
         help='Input file containing CUDA kernel relative paths.')
+    parser.add_argument('--csv-file',type=str, required=False, default="GV_TimeMem.csv")
     args = parser.parse_args()
     files = args.input_file
 
 
-    GV_cmd = ["gvtester.py","--from-file",files,"--time-as-csv","--csv-file",,"."]
+    GV_cmd = ["gvtester.py","--from-file",files,"--time-as-csv","--csv-file",args.csv_file,"--threads","1","."]
 
-    FAIAL_cmd = ["faialtester.py",files]
+    FAIAL_cmd = ["faialtester.py","--from-file",files]
 
-    subprocess.call(GV_cmd)
-    #subprocess.call(FAIAL_cmd)
+    #subprocess.call(GV_cmd)
+    subprocess.call(FAIAL_cmd)
 
 if __name__ == '__main__':
   main(sys.argv[1:])
--- faialtester.py
+++ faialtester.py
@@ -5,24 +5,45 @@ import argparse
 import subprocess
 import time
 import psutil
+import csv
 
 
 ARGUMENTS =[
     "--time-as-csv",
 ]
 
+def get_time(kernel_dir):
+    faial_cmd = ["check-drf",kernel_dir[1:-1]]
+    #print(faial_cmd)
+    start=time.time()
+    subprocess.call(faial_cmd,stdout=subprocess.DEVNULL)
+    end=time.time()
+    return (end - start)
+
 def main(arg):
     parser = argparse.ArgumentParser(description="Frontend script to run Faial on CAV2014 dataset.")
-    cmds=["check-drf",]
-    cmds.append(arg[0])
-    start=time.time()
-    subprocess.call(cmds,stdout=subprocess.DEVNULL)
-    end=time.time()
+    parser.add_argument('--from-file',type=str, required=True)
+    args = parser.parse_args()
+
+    text_file = open(args.from_file, "r")
+
+    with open("Faial_TimeMem.csv", "w+") as output_file:
+        output_file = csv.writer(output_file, delimiter=',')
+        for line in text_file:
+            current_time = get_time(line)
+            output_file.writerow([line,current_time])
+
+    text_file.close()
+
+
     #virtual_memory=psutil.virtual_memory()
     #disk_usage=psutil.disk_usage('/')
 
     #print("Total time: {}\nMemory used: {}\nDisk used: {}\
     #    ".format(end-start, virtual_memory, disk_usage ))
+
+
+
 
 if __name__ == '__main__':
   sys.exit(main(sys.argv[1:]))
