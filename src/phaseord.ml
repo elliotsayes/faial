0c5056ee39731de230167ddc20c373c986f7309e
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-07-06 18:41:28 -0400

    Optimize phase ord.

--- src/phaseord.ml
+++ src/phaseord.ml
@@ -5,7 +5,27 @@ type 'x exp =
   | Add of 'x exp * 'x exp
   | Mult of 'x exp * 'x exp
 
-let inc e = Add (Num 1, e)
+let add n1 n2 =
+  match n1, n2 with
+  | Num 0, n | n, Num 0 -> n
+  | Num n1, Num n2 -> Num (n1 + n2)
+  | _, _ -> Add (n1, n2)
+
+let mul n1 n2 =
+  match n1, n2 with
+  | Num 0, _ | _, Num 0 -> Num 0
+  | Num 1, n | Num 1, n -> n
+  | Num n1, Num n2 -> Num (n1 * n2)
+  | _, _ -> Mult (n1, n2)
+
+let inc e = add (Num 1) e
+
+let rec str_of e =
+  match e with
+  | Num x -> string_of_int x
+  | Var x -> "?"
+  | Add (x,y) -> (str_of x) ^ "+" ^ (str_of y)
+  | Mult (x, y) -> (str_of x) ^ "*" ^ (str_of y)
 
 type ('var, 's) prog =
   | Step of 's
@@ -16,12 +36,10 @@ type ('var, 's) prog =
 
 let rec infer_loop_steps e =
   match e with
-  | Skip
-  | Step _
-    -> Num 0
+  | Skip | Step _ -> Num 0
   | Sync -> Num 1
-  | Loop (_, ub, body) -> Mult (ub, infer_loop_steps body)
-  | Seq (e1, e2) -> Add (infer_loop_steps e1, infer_loop_steps e2)
+  | Loop (_, ub, body) -> mul ub (infer_loop_steps body)
+  | Seq (e1, e2) -> add (infer_loop_steps e1) (infer_loop_steps e2)
 
 type ('var, 'a) step = ('var exp * 'a)
 
@@ -30,6 +48,7 @@ type ('var, 'a) step = ('var exp * 'a)
   *)
 let extract_steps e =
   let rec iter e (p,accum)  =
+    let upper_bound e = iter e (Num 0, []) |> fst in
     match e with
     | Skip -> p, accum
     | Step d -> p, (p, d)::accum
@@ -37,10 +56,9 @@ let extract_steps e =
     | Seq (e1, e2) ->
       iter e2 (iter e1 (p, accum))
     | Loop (var, ub, body) ->
-      let loop_step = infer_loop_steps body in
-      let new_p = Add (p, Mult (Var var, loop_step)) in
+      let loop_step = upper_bound body in
+      let new_p = add p (mul (Var var) loop_step) in
       let (_, accum) = iter body (new_p, accum) in
-      Add (p, Mult(loop_step, ub)), accum
+      (add p (mul loop_step ub), accum)
   in
   iter e (Num 0, []) |> snd |> List.rev
-
