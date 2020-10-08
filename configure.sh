922764faf61c2cff98331215663526a9a844e198
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-10-08 14:28:28 -0400

    Updated container scripts.

--- configure.sh
+++ configure.sh
@@ -2,9 +2,9 @@
 if which opam > /dev/null; then
     opam install \
         ocamlbuild=0.14.0 \
-        menhir=20181113 \
-        sexplib=v0.12.0 \
-        ounit=2.0.8 \
+        menhir=20200624 \
+        sexplib=v0.14.0 \
+        ounit=2.2.3 \
         cmdliner=1.0.3 \
         ANSITerminal=0.8.1 \
         yojson=1.7.0
