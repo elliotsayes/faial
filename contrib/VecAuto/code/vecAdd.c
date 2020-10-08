8f5eb365a9eb24b8c2e0cfddd09c9a906e6b1fb3
nandiniiys <s.nandinii25@gmail.com>
2020-10-08 12:17:37 +0000

    Update vecAdd.c

--- contrib/VecAuto/code/vecAdd.c
+++ contrib/VecAuto/code/vecAdd.c
@@ -107,9 +107,9 @@ int main(int argc, char *argv[]) {
 		fprintf(race_file, "\n__global__ void vectorAdd(float *out, float *a, float *b)\n");
 		fprintf(race_file, "{\n");
 		for (int i = 1; i <= n; i++) {
-			rep(tab, 2, race_file);
+			rep(tab, 1, race_file);
 			fprintf(race_file, "for (int %c%d = 0; %c%d < %d; %c%d++)\n", 'i', nest, 'i', nest, N, 'i', nest);
-			rep(tab, 2, race_file);
+			rep(tab, 1, race_file);
 			fprintf(race_file, "{\n");
 			rep(tab, 2, race_file);
         	fprintf(race_file, "out[threadIdx.x + %c%d] = a[%c%d] + b[%c%d];\n", 'i', nest, 'i', nest, 'i', nest);
