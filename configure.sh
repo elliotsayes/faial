1d6d3ec7ec808942d7b94a00f91347446b59438a
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-11-08 08:16:39 -0500

    Updated docker container.

--- configure.sh
+++ configure.sh
@@ -5,9 +5,11 @@ if which opam > /dev/null; then
         menhir=20200624 \
         sexplib=v0.14.0 \
         ounit=2.2.3 \
-        cmdliner=1.0.3 \
-        ANSITerminal=0.8.1 \
-        yojson=1.7.0
+        cmdliner=1.0.4 \
+        ANSITerminal=0.8.2 \
+        yojson=1.7.0 \
+        ppx_deriving=5.1 \
+        ppx_hash=v0.14.0
 else
     >&2 echo "ERROR: Install opam first!"
     exit 1
