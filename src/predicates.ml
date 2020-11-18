9badeba0d23338f98d8085d40fb1ea6f34140712
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-11-18 10:53:17 -0500

    Generalizing loop-expr.

--- src/predicates.ml
+++ src/predicates.ml
@@ -4,6 +4,8 @@ open Exp
 type t = {
   pred_name: string;
   pred_body: nexp -> bexp;
+  pred_inc: (nexp -> nexp) option;
+  pred_dec: (nexp -> nexp) option;
 }
 
 type codegen = {
@@ -33,27 +35,40 @@ let eq_nums x l : bexp =
   List.map (fun i -> n_eq x (Num i)) l
   |> b_or_ex
 
-let gen_pow base x : bexp =
+let gen_pow (base:int) (n:nexp) : bexp =
   let ub = 0xFFFFFFFF in
-  let rec pows n : int list =
+  (* Generate a list of powers *)
+  let rec pows (n:int) : int list =
     let x = pow base n in
     if x > ub then []
     else if x == ub then [x]
     else x :: pows (n + 1)
   in
-  pows 0 |> eq_nums x
+  pows 0 |> eq_nums n
 
 let all_predicates : t list =
+  let mk_uint size : t =
+    let n : string = "uint" ^(string_of_int size) in
+    let b (x:nexp) : bexp = n_le x (Num ((pow 2 size) - 1)) in
+    {
+        pred_name = n;
+        pred_body = b;
+        pred_inc = None;
+        pred_dec = None;
+    }
+  in
   List.map (fun base ->
     { pred_name = "pow" ^ string_of_int base;
-      pred_body = gen_pow base
+      pred_body = gen_pow base;
+      pred_inc = Some (fun x -> n_mult x (Num base));
+      pred_dec = Some (fun x -> n_div x (Num base));
     }
   ) (range 2 4)
   @
   [
-    { pred_name = "uint32"; pred_body = fun x -> n_le x (Num 0xFFFFFFFF) };
-    { pred_name = "uint16"; pred_body = fun x -> n_le x (Num 0xFFFF) };
-    { pred_name = "uint8"; pred_body = fun x -> n_le x (Num 0xFF) };
+      mk_uint 32;
+      mk_uint 16;
+      mk_uint 8;
   ]
 
 let make_pred_db (l:t list) : (string, t) Hashtbl.t =
@@ -69,6 +84,8 @@ let call_opt (name:string) (n:nexp) : bexp option =
   | Some p -> Some (p.pred_body n)
   | None -> None
 
+let all_predicates_db = make_pred_db all_predicates
+
 let get_predicates (b:bexp) : t list =
   let rec get_names (b:bexp) (preds:StringSet.t) : StringSet.t =
     match b with
@@ -78,7 +95,24 @@ let get_predicates (b:bexp) : t list =
     | NRel (_, _, _)
     | Bool _ -> preds
   in
-  let db = make_pred_db all_predicates in
   get_names b StringSet.empty
   |> StringSet.elements
-  |> List.map (Hashtbl.find db)
+  |> List.map (Hashtbl.find all_predicates_db)
+
+let step_inc (s:step_expr) : nexp -> nexp =
+  match s with
+  | Default n -> n_plus n
+  | StepName pred_name ->
+    let (p:t) = Hashtbl.find all_predicates_db pred_name in
+    match p.pred_inc with
+    | Some f -> f
+    | None -> failwith ("Cannot use predicate '" ^ pred_name ^ "' as a step expression.")
+
+let step_dec (s:step_expr) (e:nexp) =
+  match s with
+  | Default n -> n_minus n
+  | StepName pred_name ->
+    let (p:t) = Hashtbl.find all_predicates_db pred_name in
+    match p.pred_dec with
+    | Some f -> f
+    | None -> failwith ("Cannot use predicate '" ^ pred_name ^ "' as a step expression.")
