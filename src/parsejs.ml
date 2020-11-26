3a318ad8f77e4e8fc6641659517140dada34ef06
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2020-11-26 09:24:06 -0500

    Ignore C++ constructors and unstructured loops.

--- src/parsejs.ml
+++ src/parsejs.ml
@@ -463,9 +463,20 @@ let rec parse_stmt j =
          Some (Block [])
       | _ -> None
     );
+    "DoStmt", ([], function
+      | [] ->
+        prerr_endline ("WARNING: we can't analyze unstructure while-loops");
+        Some (Block [])
+      | _ -> None
+    );
     "WhileStmt", ([], function
       | [] ->
         prerr_endline ("WARNING: we can't analyze unstructure while-loops");
+        Some (Block [])
+      | _ -> None
+    );
+    "CXXUnresolvedConstructExpr", ([], function
+      | [] ->
         Some (Block [])
       | _ -> None
     );
