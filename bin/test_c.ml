b18dada2fd4ad2c32ca43c80496bf62870b610e9
Gregory Blike <greg.blike@inkanitolabs.com>
2022-06-18 02:51:04 -0400

    Add warning for function calls that use arrays

--- bin/test_c.ml
+++ bin/test_c.ml
@@ -3,20 +3,27 @@ module VarSet = Exp.VarSet
 module VarMap = Exp.VarMap
 
 (* ------------------------------------------------------------------------ *)
+let (get_variable: Dlang.d_exp -> Exp.variable option) = function
+    Dlang.ParmVarDecl d_var -> Some d_var.name
+  | Dlang.VarDecl d_var -> Some d_var.name
+  | _ -> None
+
 let warn_call_use_array (arrays:VarSet.t StringMap.t) (p:Dlang.d_program) : unit =
-  let known_name = fun name -> StringMap.mem name arrays in
-  let extract_arg_names = function
-      Dlang.ParmVarDecl d_var -> Some (d_var.name |> Exp.var_name)
-    | Dlang.VarDecl d_var -> Some (d_var.name |> Exp.var_name)
-    | _ -> None in
-  let var_decl_names = fun (args: Dlang.d_exp list) ->
-    args |> List.filter_map extract_arg_names |>  List.filter known_name in
-  p |> List.filter_map (function Dlang.Kernel kernel -> Some kernel | _ -> None)
+  let print_matched = fun arrays -> fun (func, args) -> args
+    |> List.filter_map get_variable
+    |> VarSet.of_list
+    |> VarSet.inter arrays
+    |> VarSet.elements
+    |> List.map Exp.var_name
+    |> List.iter (fun name -> "WARN: Function call with array: " ^ (Dlang.exp_to_s func) ^ " " ^ name |> Stdlib.prerr_endline) in
+  p |> List.filter_map (function
+        | Dlang.Kernel kernel -> StringMap.find_opt kernel.name arrays
+            |> Option.map (fun arrays -> (kernel.code, arrays))
+        | _ -> None)
     |> List.iter (
-        fun (kernel: Dlang.d_kernel) -> kernel.code
-        |> (Dlang.for_dexp_in_dstmt (function
-               | CXXOperatorCallExpr expr -> expr.args |> var_decl_names |> List.iter print_endline
-               | CallExpr expr -> expr.args |> var_decl_names |> List.iter print_endline
+        fun (code, arrays) -> code |> (Dlang.for_dexp_in_dstmt (function
+               | CXXOperatorCallExpr expr -> (expr.func, expr.args) |> print_matched arrays
+               | CallExpr expr -> (expr.func, expr.args) |> print_matched arrays
                | _ -> ()
              )
            )
@@ -26,7 +33,7 @@ let make_array_map (ks:Imp.p_kernel list) : VarSet.t StringMap.t =
   ks
   |> List.map (fun k ->
       let open Imp in
-      let arrays = 
+      let arrays =
         (* Convert map from name to attribute, to set of name *)
         VarMap.bindings k.p_kernel_arrays
         |> List.split
@@ -60,16 +67,17 @@ let analyze (j:Yojson.Basic.t) : Cast.c_program  * Dlang.d_program * (Imp.p_kern
     exit(-1)
 
 
-let () =
+let () = (*k1 c k2 d k3 imp *)
   let (k1, k2, k3) =
     Yojson.Basic.from_channel stdin
     |> analyze
   in
   warn_call_use_array (make_array_map k3) k2;
-  (*
+
   Cast.print_program k1;
   print_endline "------";
   Dlang.print_program k2;
+  print_endline "";
+  k2 |> List.length |> print_int;
   print_endline "-------";
-  *)
-  (* List.iter Imp.print_kernel k3 *)
+  List.iter Imp.print_kernel k3
