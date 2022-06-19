09de4d3b16ed9b1903b13425def59bed44133df4
udaya <u.sathiyamoorthy001@umb.edu>
2022-06-19 12:09:12 -0400

    Updated file parsing functions

--- bin/cgen.ml
+++ bin/cgen.ml
@@ -1,31 +1,31 @@
 open Exp
 open Proto
-open Common
 open Serialize
 
 (* ----------------- constants -------------------- *)
 let thread_globals : VarSet.t =
-  VarSet.of_list (List.map var_make
-  ["gridDim.x"; "blockIdx.x"; "blockDim.x";
-  "gridDim.y"; "blockIdx.y"; "blockDim.y";
-  "gridDim.z"; "blockIdx.z"; "blockDim.z"])
+  (List.map var_make 
+     ["blockDim.x"; "blockIdx.x"; "gridDim.x"; "gridIdx.x"; "threadDim.x";
+      "blockDim.y"; "blockIdx.y"; "gridDim.y"; "gridIdx.y"; "threadDim.y";
+      "blockDim.z"; "blockIdx.z"; "gridDim.z"; "gridIdx.z"; "threadDim.z"])
+  |> VarSet.of_list
 
 (* ----------------- serialization -------------------- *)
 
 (* Gives the dummy variable string for any variable *)
-let var_to_dummy (v:variable) : string =
+let var_to_dummy (v : variable) : string =
   "__dummy" ^ var_name v
 
 (* Maps a list of values to an index-separated string containing them *)
-let idx_to_s (f:'a -> string) (l:'a list) : string =
+let idx_to_s (f : 'a -> string) (l : 'a list) : string =
   "[" ^ (Common.join "][" (List.map f l)) ^ "]"
-  
+
 (* Gives the dummy instruction string for any array read/write *)
 let acc_expr_to_dummy (x, a) : PPrint.t list =
   let var = var_name x ^ idx_to_s PPrint.n_to_s a.access_index in
   [Line (match a.access_mode with
-  | R -> var_to_dummy x ^ " = " ^ var ^ ";"
-  | W -> var ^ " = " ^ var_to_dummy x ^ "_w();")]
+       | R -> var_to_dummy x ^ " = " ^ var ^ ";"
+       | W -> var ^ " = " ^ var_to_dummy x ^ "_w();")]
 
 (* Converts source instruction to a valid CUDA operation *)
 let rec inst_to_s : inst -> PPrint.t list =
@@ -39,105 +39,108 @@ let rec inst_to_s : inst -> PPrint.t list =
       Line "}"
     ]
   | Loop (r, p) ->
-    [
+    [ 
       Line ("for (" ^ "int " ^ ident r.range_var ^ " = " ^
-      n_to_s r.range_lower_bound ^ "; " ^ ident r.range_var ^ " < " ^
-      n_to_s r.range_upper_bound ^ "; " ^ ident r.range_var ^ " = " ^
-      ident r.range_var ^ " " ^ s_to_s r.range_step ^ ") {");
+            n_to_s r.range_lower_bound ^ "; " ^ ident r.range_var ^ " < " ^
+            n_to_s r.range_upper_bound ^ "; " ^ ident r.range_var ^ " = " ^
+            ident r.range_var ^ " " ^ s_to_s r.range_step ^ ") {");
       Block (List.map inst_to_s p |> List.flatten);
-      Line "}"
+      Line "}" 
     ]
 
-let prog_to_s (p: prog) : PPrint.t list =
+let prog_to_s (p : prog) : PPrint.t list =
   List.map inst_to_s p |> List.flatten
 
 (* Get the type of an array, defaulting to int if it is unknown *)
-let arr_type (arr:array_t) : string =
+let arr_type (arr : array_t) : string =
   match arr.array_type with
   | [] -> "int"
   | _ -> Common.join " " arr.array_type
 
 (* Helper functions for making the kernel header/parameters *)
-let global_arr_to_l (vs:array_t VarMap.t) : string list =
+let global_arr_to_l (vs : array_t VarMap.t) : string list =
   VarMap.bindings vs
-  |> List.map (fun (k,v) -> arr_type v ^ " *" ^ var_name k)
+  |> List.map (fun (k, v) -> arr_type v ^ " *" ^ var_name k)
 
-let global_var_to_l (vs:VarSet.t) : string list =
+let global_var_to_l (vs : VarSet.t) : string list =
   VarSet.elements (VarSet.diff vs thread_globals)
   |> List.map (fun v -> "int " ^ var_name v)
 
 (* Helper functions for making kernel variable declarations/prototypes *)
-let arr_to_proto (vs:array_t VarMap.t) : PPrint.t list =
-  VarMap.bindings vs
-  |> List.map (fun (k,v) -> PPrint.Line ("extern " ^
-  arr_type v ^ " " ^ var_to_dummy k ^ "_w();"))
+let arr_to_proto (vs : array_t VarMap.t) : PPrint.t list =
+  VarMap.bindings vs 
+  |> List.map (fun (k, v) ->
+      PPrint.Line ("extern " ^ arr_type v ^ " " ^ var_to_dummy k ^ "_w();"))
 
-let arr_to_shared (vs:array_t VarMap.t) : PPrint.t list = 
+let arr_to_shared (vs : array_t VarMap.t) : PPrint.t list =
   VarMap.bindings vs
-  |> List.map (fun (k,v) -> PPrint.Line ((match v.array_size with
-  | [] -> "extern " | _ -> "") ^ "__shared__ " ^ arr_type v ^ " " ^
-  var_name k ^ idx_to_s string_of_int v.array_size ^ ";"))
+  |> List.map (fun (k, v) -> 
+      PPrint.Line ((match v.array_size with | [] -> "extern " | _ -> "") 
+                   ^ "__shared__ " ^ arr_type v ^ " " ^ var_name k
+                   ^ idx_to_s string_of_int v.array_size ^ ";"))
 
-let arr_to_dummy (vs:array_t VarMap.t) : PPrint.t list =
+let arr_to_dummy (vs : array_t VarMap.t) : PPrint.t list =
   VarMap.bindings vs
-  |> List.map (fun (k,v) -> PPrint.Line (arr_type v ^ " " ^ var_to_dummy k ^ ";"))
+  |> List.map (fun (k, v) -> 
+      PPrint.Line (arr_type v ^ " " ^ var_to_dummy k ^ ";"))
 
-(* Serialization of the kernel *)
-let body_to_s (f:'a -> PPrint.t list) (k:'a kernel) : PPrint.t =
+let body_to_s (f : 'a -> PPrint.t list) (k : 'a kernel) : PPrint.t =
   let funct_protos = arr_to_proto k.kernel_arrays in
   let shared_arr = arr_to_shared (VarMap.filter (fun k -> fun v ->
-  v.array_hierarchy = SharedMemory) k.kernel_arrays) in
+      v.array_hierarchy = SharedMemory) k.kernel_arrays) in
   let dummy_var = arr_to_dummy k.kernel_arrays in
   PPrint.Block (funct_protos @ shared_arr @ dummy_var @ (f k.kernel_code))
 
-let kernel_to_s (f:'a -> PPrint.t list) (k:'a kernel) : PPrint.t list =
+(* Serialization of the kernel *)
+let kernel_to_s (f : 'a -> PPrint.t list) (k : 'a kernel) : PPrint.t list =
   let open PPrint in
-  let kernel_name = if k.kernel_name = "main" then "kernel" else k.kernel_name in
+  let k_name = if k.kernel_name = "main" then "kernel" else k.kernel_name in
   let global_arr = global_arr_to_l (VarMap.filter (fun k -> fun v ->
-  v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
+      v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
   let global_var = global_var_to_l k.kernel_global_variables in
   [
     Line "__global__";
-    Line ("void " ^ kernel_name ^ "(" ^ Common.join
-    ", " (global_arr @ global_var) ^ ")");
+    Line ("void " ^ k_name ^ "(" ^ Common.join
+            ", " (global_arr @ global_var) ^ ")");
     Line "{";
     (body_to_s f k);
     Line "}"
   ]
-    
-let print_kernel (f:'a -> PPrint.t list) (k: 'a kernel) : unit =
+
+let print_kernel (f : 'a -> PPrint.t list) (k : 'a kernel) : unit =
   PPrint.print_doc (kernel_to_s f k)
 
-let print_k (k:prog kernel) : unit =
+let print_k (k : prog kernel) : unit =
   PPrint.print_doc (kernel_to_s prog_to_s k)
 
 (* Kernel to TOML conversion *)
-let global_arr_to_tlist (vs:array_t VarMap.t) : Toml.Types.table list =
-  VarMap.bindings vs
-  |> List.map (fun (k,v) -> Toml.Min.of_key_values
-  [Toml.Min.key (var_name k), Toml.Types.TString (arr_type v)])
-
-let global_var_to_tlist (vs:VarSet.t) : Toml.Types.table list =
-  VarSet.elements (VarSet.diff vs thread_globals)
-  |> List.map (fun v -> Toml.Min.of_key_values
-  [Toml.Min.key (var_name v), Toml.Types.TString "int"])
-
-let kernel_to_toml (k:prog kernel) : Toml.Types.table =
-  let global_arr = (VarMap.filter (fun k -> fun v ->
-  v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
+let arrays_to_l (vs : array_t VarMap.t) : Toml.Types.table list =
   let open Toml.Min in
   let open Toml.Types in
-  of_key_values
+  VarMap.bindings vs
+  |> List.map (fun (k, v) ->
+      of_key_values [key (var_name k), TString (arr_type v)])
+
+let scalars_to_l (vs : VarSet.t) : Toml.Types.table list =
+  let open Toml.Min in
+  let open Toml.Types in
+  VarSet.elements (VarSet.diff vs thread_globals)
+  |> List.map (fun v -> of_key_values [key (var_name v), TString "int"])
+
+let kernel_to_toml (k : prog kernel) : Toml.Types.table =
+  let global_arr = (VarMap.filter (fun k -> fun v ->
+      v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
+  let open Toml.Min in
+  let open Toml.Types in
+  let open PPrint in
   [
     key "pass", TBool true;
     key "includes", TArray (NodeInt []);
-    key "body", TString (PPrint.doc_to_string (Line "" :: [body_to_s prog_to_s k]));
-    key "arrays", TArray (NodeTable (global_arr_to_tlist global_arr));
-    key "scalars", TArray (NodeTable (global_var_to_tlist k.kernel_global_variables));
+    key "body", TString (doc_to_string (Line "" :: [body_to_s prog_to_s k]));
+    key "arrays", TArray (NodeTable (arrays_to_l global_arr));
+    key "scalars", TArray (NodeTable (scalars_to_l k.kernel_global_variables));
   ]
+  |> of_key_values
 
-let print_toml (t:Toml.Types.table) : unit =
-  print_string (Toml.Printer.string_of_table t)
-
-let print_toml_body (t:Toml.Types.table) : unit =
-  print_string (Toml.Printer.string_of_table t)
+let print_toml (table : Toml.Types.table) : unit =
+  print_string (Toml.Printer.string_of_table table)
--- bin/prototocuda.ml
+++ bin/prototocuda.ml
@@ -1,101 +1,79 @@
-open Proto
 open Cmdliner
 
-(* ----------------- kernel functions -------------------- *)
+(* Parse C AST as JSON using cu-to-json *)
+let cu_to_json (fname : string) : string =
+  let cmd = "cu-to-json " ^ fname in
+  let in_c = Unix.open_process_in cmd in
+  let raw_json_ast = In_channel.input_all in_c in
+  In_channel.close in_c; raw_json_ast
 
-let open_ic_with (fname:string option) (f : in_channel -> unit) : unit =
-  let ic, (closer: in_channel -> unit) = match fname with
-    | Some fname -> (open_in fname, close_in_noerr)
-    | None -> (stdin, fun x -> ())
-  in
-  try (f ic; closer ic) with
-  | e -> closer ic;
-    raise e
-
-let p_kernel_parser fname input : prog kernel =
-  let fname = match fname with
-    | Some x -> x
-    | None -> "<STDIN>"
-  in
-  let filebuf = Lexing.from_channel input in
-  Scan.set_filename filebuf fname;
-  try Parse2.main Scan.read filebuf with
-  | Parse2.Error ->
-    let b = Buffer.create 1024 in
-    let sloc = Sourceloc.of_lexbuf filebuf in
-    Printf.bprintf b "%a: syntax error" Sourceloc.location_bprint_start sloc;
-    (try
-       Printf.bprintf b "%a" Sourceloc.location_bprint_title sloc
-     with
-       Sys_error _ -> ()
-    );
-    raise (Common.ParseError b)
-
-let j_kernel_parser (_:string option) ic =
-  try Yojson.Basic.from_channel ic |> Parsejs.parse_kernels.run with
+let parse_json (raw_json_ast : string) : Yojson.Basic.t =
+  try Yojson.Basic.from_string raw_json_ast with
   | Yojson.Json_error("Blank input data") ->
     (* If the input is blank, just ignore the input and err with -1 *)
     raise (Common.mk_parse_error_s "Empty input data. Blank file?\n")
   | Yojson.Json_error(e) ->
     raise (Common.mk_parse_error_s (Printf.sprintf "Error parsing JSON: %s\n" e))
 
-type i_kernel =
-  | JKernel of Imp.p_kernel list
-  | PKernel of prog kernel
-
-let parse_i_kernel (use_json:bool) (fname:string option) (ic:in_channel) : i_kernel =
-  if use_json then
-    JKernel (j_kernel_parser fname ic)
+(* Main function *)
+let read_kernels (fname : string) (use_cuda : bool) =
+  if use_cuda then
+    (* For parsing CUDA files: *)
+    try
+      let raw_json = cu_to_json fname in
+      let parsed_json = parse_json raw_json in
+      let c_ast = parsed_json |> Cast.parse_program |> Result.get_ok in
+      let d_ast = c_ast |> Dlang.rewrite_program in
+      let imp = d_ast |> D_to_imp.parse_program |> Result.get_ok in
+      imp |> List.map Imp.compile
+    with
+    | Common.ParseError b ->
+      Buffer.output_buffer stderr b;
+      exit (-1)
   else
-    PKernel (p_kernel_parser fname ic)
+    (* For parsing protocols: *)
+    let filebuf = Lexing.from_channel (open_in fname) in
+    try [Parse2.main Scan.read filebuf] with
+    | Parse2.Error ->
+      let sloc = Sourceloc.of_lexbuf filebuf in
+      let b = Buffer.create 1024 in
+      Printf.bprintf b "%a: syntax error\n" Sourceloc.location_bprint_start sloc;
+      (try
+         Printf.bprintf b "\n%a" Sourceloc.location_bprint_title sloc
+       with
+         Sys_error _ -> ());
+      Buffer.output_buffer stderr b;
+      exit (-1)
 
-let open_i_kernel_with (use_json:bool) (fname:string option) (f:i_kernel -> unit) : unit =
-  open_ic_with fname (fun ic ->
-      f (parse_i_kernel use_json fname ic)
-    )
-
-let i_kernel_to_p_kernel (k:i_kernel) : prog kernel list =
-  match k with
-  | JKernel ks -> List.map Imp.compile ks
-  | PKernel p -> [p]
-
-(* ----------------- command line argument parser -------------------- *)
-
+(* Command-line interface *)
 let p2c_t =
   let get_fname =
     let doc = "The path $(docv) of the GPU contract." in
-    Arg.(value & pos 0 (some string) None & info [] ~docv:"CONTRACT" ~doc)
+    Arg.(required & pos 0 (some file) None & info [] ~docv:"FILENAME" ~doc)
   in
-
-  let use_json =
-    let doc = "Parse a JSON file" in
-    Arg.(value & flag & info ["json"] ~doc)
+  let use_cuda =
+    let doc = "Parse a CUDA file" in
+    Arg.(value & flag & info ["cuda"] ~doc)
   in
-
   let output_toml =
     let doc = "Output a TOML file" in
     Arg.(value & flag & info ["toml"] ~doc)
   in
-
   let do_p2c
-      (fname: string option)
-      (use_json: bool)
-      (output_toml: bool) : unit =
+      (fname : string)
+      (use_cuda : bool)
+      (output_toml : bool) : unit =
     let open Cgen in
-    let print_cuda (k : i_kernel) : unit =
-      if output_toml then
-        List.iter (fun k -> print_toml (kernel_to_toml k)) (i_kernel_to_p_kernel k)
-      else
-        List.iter (fun k -> print_k k) (i_kernel_to_p_kernel k)
-    in
-    try open_i_kernel_with use_json fname print_cuda with
-    | Common.ParseError b ->
-      Buffer.output_buffer stderr b;
-      exit (-1) in
+    let kernels = read_kernels fname use_cuda in
+    if output_toml then
+      List.iter (fun k -> print_toml (kernel_to_toml k)) kernels
+    else
+      List.iter (fun k -> print_k k) kernels
+  in
   Term.(
     const do_p2c
     $ get_fname
-    $ use_json
+    $ use_cuda
     $ output_toml
   )
 
