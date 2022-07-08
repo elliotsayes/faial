1fcd908f948a93869dc40f90369b8bb76991b448
udaya <u.sathiyamoorthy001@umb.edu>
2022-07-08 09:41:26 -0400

    Replaced cu-to-json code with call to module

--- bin/prototocuda.ml
+++ bin/prototocuda.ml
@@ -1,49 +1,37 @@
 open Cmdliner
 
-(* Parse C AST as JSON using cu-to-json *)
-let cu_to_json (fname : string) : string =
-  let cmd = "cu-to-json " ^ fname in
-  let in_c = Unix.open_process_in cmd in
-  let raw_json_ast = In_channel.input_all in_c in
-  In_channel.close in_c; raw_json_ast
+(* For parsing CUDA files *)
+let parse_cuda (fname : string) =
+  try
+    let parsed_json = Cu_to_json.cu_to_json fname in
+    let c_ast = parsed_json |> Cast.parse_program |> Result.get_ok in
+    let d_ast = c_ast |> Dlang.rewrite_program in
+    let imp = d_ast |> D_to_imp.parse_program |> Result.get_ok in
+    imp |> List.map Imp.compile
+  with
+  | Common.ParseError b ->
+    Buffer.output_buffer stderr b;
+    exit (-1)
 
-let parse_json (raw_json_ast : string) : Yojson.Basic.t =
-  try Yojson.Basic.from_string raw_json_ast with
-  | Yojson.Json_error("Blank input data") ->
-    (* If the input is blank, just ignore the input and err with -1 *)
-    raise (Common.mk_parse_error_s "Empty input data. Blank file?\n")
-  | Yojson.Json_error(e) ->
-    raise (Common.mk_parse_error_s (Printf.sprintf "Error parsing JSON: %s\n" e))
+(* For parsing protocols *)
+let parse_proto (fname : string) =
+  let filebuf = Lexing.from_channel (open_in fname) in
+  try [Parse2.main Scan.read filebuf] with
+  | Parse2.Error ->
+    let sloc = Sourceloc.of_lexbuf filebuf in
+    let b = Buffer.create 1024 in
+    Printf.bprintf b "%a: syntax error\n" Sourceloc.location_bprint_start sloc;
+    (try
+       Printf.bprintf b "\n%a" Sourceloc.location_bprint_title sloc
+     with
+       Sys_error _ -> ());
+    Buffer.output_buffer stderr b;
+    exit (-1)
 
 (* Main function *)
 let read_kernels (fname : string) (use_cuda : bool) =
-  if use_cuda then
-    (* For parsing CUDA files: *)
-    try
-      let raw_json = cu_to_json fname in
-      let parsed_json = parse_json raw_json in
-      let c_ast = parsed_json |> Cast.parse_program |> Result.get_ok in
-      let d_ast = c_ast |> Dlang.rewrite_program in
-      let imp = d_ast |> D_to_imp.parse_program |> Result.get_ok in
-      imp |> List.map Imp.compile
-    with
-    | Common.ParseError b ->
-      Buffer.output_buffer stderr b;
-      exit (-1)
-  else
-    (* For parsing protocols: *)
-    let filebuf = Lexing.from_channel (open_in fname) in
-    try [Parse2.main Scan.read filebuf] with
-    | Parse2.Error ->
-      let sloc = Sourceloc.of_lexbuf filebuf in
-      let b = Buffer.create 1024 in
-      Printf.bprintf b "%a: syntax error\n" Sourceloc.location_bprint_start sloc;
-      (try
-         Printf.bprintf b "\n%a" Sourceloc.location_bprint_title sloc
-       with
-         Sys_error _ -> ());
-      Buffer.output_buffer stderr b;
-      exit (-1)
+  if use_cuda then parse_cuda fname
+  else parse_proto fname
 
 (* Command-line interface *)
 let p2c_t =
