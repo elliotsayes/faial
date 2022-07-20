b7ee5ed6c4a0eadf692c0d6781db92580007c81a
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2022-07-20 01:30:39 -0400

    Added a UI to next-gen.

--- bin/next_gen.ml
+++ bin/next_gen.ml
@@ -1,7 +1,10 @@
 module StringMap = Common.StringMap
 module VarSet = Exp.VarSet
 module VarMap = Exp.VarMap
-
+module Environ = Z3_solver.Environ
+module Witness = Z3_solver.Witness
+module Vec3 = Z3_solver.Vec3
+module Task = Z3_solver.Task
 
 let parse_imp (j:Yojson.Basic.t) : Imp.p_kernel list =
   let open Indexflow in
@@ -25,6 +28,45 @@ let parse_imp (j:Yojson.Basic.t) : Imp.p_kernel list =
     Rjson.print_error e;
     exit(-1)
 
+let box_environ (e:Environ.t) : PrintBox.t =
+  PrintBox.(
+    v_record (List.map (fun (k,v) -> (k, text v)) e)
+    |> frame
+  )
+
+let vec_to_s (v: Vec3.t) : string =
+  "x = " ^ v.x ^ ", y = " ^ v.y ^ ", z = " ^ v.z
+
+let box_tasks (t1:Task.t) (t2:Task.t) : PrintBox.t =
+  let open PrintBox in
+  let locals =
+    t1.locals
+    |> List.map (fun (k, v1) -> 
+      [| text k; text v1; text (List.assoc k t2.locals) |]
+    )
+  in
+  let locals = [| text "threadIdx"; text @@ vec_to_s t1.thread_idx; text @@ vec_to_s t2.thread_idx |] :: locals
+  |> Array.of_list
+in
+  grid locals |> frame
+
+let box_globals (w:Witness.t) : PrintBox.t =
+  [
+    "index", "[" ^ Common.join ", " w.indices ^ "]";
+    "blockDim", vec_to_s w.block_dim;
+    "blockIdx", vec_to_s w.block_idx;
+    "gridDim", vec_to_s w.grid_dim;
+  ] @
+  w.globals
+  |> box_environ
+
+let box_locals (w:Witness.t) : PrintBox.t =
+  let (t1, t2) = w.tasks in
+  box_tasks t1 t2
+
+let print_box: PrintBox.t -> unit =
+  PrintBox_text.output stdout
+
 
 let main (fname: string) : unit =
   let j = Cu_to_json.cu_to_json ~ignore_fail:true fname in
@@ -41,7 +83,17 @@ let main (fname: string) : unit =
     let open Solution in
     solve p
     |> Streamutil.iter (fun s ->
-      Solution.to_json s |> Yojson.Basic.pretty_to_channel stdout
+      match s with
+      | Drf -> print_endline "drf"
+      | Unknown -> print_endline "unknown"
+      | Racy w ->
+        print_endline "Globals";
+        box_globals w |> print_box;
+        print_endline "\n\nLocals";
+        box_locals w |> print_box;
+        print_endline "";
+      ;
+      (* Solution.to_json s |> Yojson.Basic.pretty_to_channel stdout *)
     )
   ) p
 
