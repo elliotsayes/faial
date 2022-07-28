3085d3ca49e29b86a10acadceaba347d2bb52ba3
udaya <u.sathiyamoorthy001@umb.edu>
2022-07-28 17:03:30 -0400

    Moved function prototypes outside of kernel body

--- bin/cgen.ml
+++ bin/cgen.ml
@@ -36,6 +36,9 @@ let vector_types : Common.StringSet.t =
    "short"; "uchar"; "uint"; "ulong"; "ulonglong"; "ushort";]
   |> Common.StringSet.of_list
 
+let base_protos : string list =
+  ["extern __device__ int __dummy_int();"]
+
 (* ----------------- serialization -------------------- *)
 
 (* Gives the dummy variable string for any variable *)
@@ -46,7 +49,7 @@ let var_to_dummy (v : variable) : string =
 let idx_to_s (f : 'a -> string) (l : 'a list) : string =
   "[" ^ (Common.join "][" (List.map f l |> List.rev)) ^ "]"
 
-(* Gives the dummy instruction string for any array read/write *)
+(* Gives the dummy instruction for any array read/write *)
 let acc_expr_to_dummy (x, a) : PPrint.t list =
   let var = var_name x ^ idx_to_s PPrint.n_to_s a.access_index in
   [Line (match a.access_mode with
@@ -84,6 +87,12 @@ let arr_type (arr : array_t) (strip_const : bool) : string =
       (List.filter (fun x -> x <> "const") arr.array_type)
   else Common.join " " arr.array_type
 
+(* Create external function prototypes for writing to each array *)
+let arr_to_proto (vm : array_t VarMap.t) : string list =
+  VarMap.bindings vm 
+  |> List.map (fun (k, v) -> ("extern __device__ " ^ arr_type v true
+                              ^ " " ^ var_to_dummy k ^ "_w();"))
+
 (* Checks if a string is a known C++/CUDA type *)
 let is_known_type (s : string) : bool =
   if Common.StringSet.mem s cpp_types then true
@@ -94,8 +103,7 @@ let is_known_type (s : string) : bool =
              | '1' | '2' | '3' | '4' -> true
              | _ -> false
 
-(* Helper functions for making the kernel header/parameters *)
-let declare_unknown_types (vm : array_t VarMap.t) =
+let declare_unknown_types (vm : array_t VarMap.t) : string list =
   (* Converts an array type to a declaration, or None if it is a known type *)
   let rec arr_type_to_decl = function
     | [] -> None
@@ -107,10 +115,7 @@ let declare_unknown_types (vm : array_t VarMap.t) =
   List.filter_map (fun (k, v) -> arr_type_to_decl v.array_type) |>
   (* Remove duplicates *)
   Common.StringSet.of_list |>
-  Common.StringSet.elements |>
-  function
-  | [] -> PPrint.Nil
-  | types -> PPrint.Line (Common.join "\n" types)
+  Common.StringSet.elements
 
 let global_arr_to_l (vm : array_t VarMap.t) : string list =
   VarMap.bindings vm
@@ -120,12 +125,7 @@ let global_var_to_l (vs : VarSet.t) : string list =
   VarSet.elements (VarSet.diff vs thread_globals)
   |> List.map (fun v -> "int " ^ var_name v)
 
-(* Helper functions for making kernel variable declarations/prototypes *)
-let arr_to_proto (vm : array_t VarMap.t) : PPrint.t list =
-  VarMap.bindings vm 
-  |> List.map (fun (k, v) -> PPrint.Line ("extern " ^ arr_type v true
-                                          ^ " " ^ var_to_dummy k ^ "_w();"))
-
+(* Helper functions for making kernel variable declarations *)
 let arr_to_shared (vm : array_t VarMap.t) : PPrint.t list =
   VarMap.bindings vm
   |> List.map (fun (k, v) -> 
@@ -140,36 +140,31 @@ let arr_to_dummy (vm : array_t VarMap.t) : PPrint.t list =
 
 let local_var_to_l (vs : VarSet.t) : PPrint.t list =
   (* A local variable must not be a tid/dummy variable *)
-  let local_var = VarSet.filter
-      (fun v -> not (VarSet.mem v thread_locals
-                     || String.starts_with "__dummy" (var_name v))) vs
-                  |> VarSet.elements in
-  (* Generate a single prototype to initialize local variables with *)
-  match local_var with
-  | [] -> []
-  | _ -> PPrint.Line "extern int __dummy_int();" :: List.map
-           (fun v -> PPrint.Line ("int " ^ var_name v ^ " = __dummy_int();"))
-           local_var
+  VarSet.filter (fun v -> not (VarSet.mem v thread_locals ||
+                               String.starts_with "__dummy" (var_name v)))
+    vs |>
+  VarSet.elements |>
+  (* Use a single function to initialize all local variables *)
+  List.map (fun v -> PPrint.Line ("int " ^ var_name v ^ " = __dummy_int();"))
 
 let body_to_s (f : 'a -> PPrint.t list) (k : 'a kernel) : PPrint.t =
   let shared_arr = arr_to_shared (VarMap.filter (fun k -> fun v ->
       v.array_hierarchy = SharedMemory) k.kernel_arrays) in
-  let funct_protos = arr_to_proto k.kernel_arrays in
   let local_var = local_var_to_l k.kernel_local_variables in
   let dummy_var = arr_to_dummy k.kernel_arrays in
-  PPrint.Block (shared_arr @ funct_protos @ local_var @ dummy_var
-                @ (f k.kernel_code))
+  PPrint.Block (shared_arr @ local_var @ dummy_var @ (f k.kernel_code))
 
 (* Serialization of the kernel *)
 let kernel_to_s (f : 'a -> PPrint.t list) (k : 'a kernel) : PPrint.t list =
   let open PPrint in
-  let decl_types = declare_unknown_types k.kernel_arrays in
+  let funct_protos = base_protos @ (arr_to_proto k.kernel_arrays) in
+  let unknown_type_decls = declare_unknown_types k.kernel_arrays in
   let k_name = if k.kernel_name = "main" then "kernel" else k.kernel_name in
   let global_arr = global_arr_to_l (VarMap.filter (fun k -> fun v ->
       v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
   let global_var = global_var_to_l k.kernel_global_variables in
   [
-    decl_types;
+    Line (Common.join "\n" (funct_protos @ unknown_type_decls));
     Line "__global__";
     Line ("void " ^ k_name ^ "(" ^ Common.join
             ", " (global_arr @ global_var) ^ ")");
@@ -185,24 +180,21 @@ let print_k (k : prog kernel) : unit =
   PPrint.print_doc (kernel_to_s prog_to_s k)
 
 (* Kernel to TOML conversion *)
+open Toml.Min
+open Toml.Types
+
 let arrays_to_l (vm : array_t VarMap.t) : Toml.Types.table list =
-  let open Toml.Min in
-  let open Toml.Types in
   VarMap.bindings vm
   |> List.map (fun (k, v) ->
       of_key_values [key (var_name k), TString (arr_type v false)])
 
 let scalars_to_l (vs : VarSet.t) : Toml.Types.table list =
-  let open Toml.Min in
-  let open Toml.Types in
   VarSet.elements (VarSet.diff vs thread_globals)
   |> List.map (fun v -> of_key_values [key (var_name v), TString "int"])
 
 let kernel_to_toml (k : prog kernel) : Toml.Types.table =
   let global_arr = (VarMap.filter (fun k -> fun v ->
       v.array_hierarchy = GlobalMemory) k.kernel_arrays) in
-  let open Toml.Min in
-  let open Toml.Types in
   let open PPrint in
   [
     key "pass", TBool true;
