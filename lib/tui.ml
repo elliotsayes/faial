ce7784c815d339dc2463f847853fb52ea886fcb8
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2022-09-18 17:29:11 -0400

    Use updated interface of location.

--- lib/tui.ml
+++ lib/tui.ml
@@ -3,28 +3,19 @@ let make_bold ?(color=[ANSITerminal.Foreground ANSITerminal.Red]) =
 
 module LocationUI = struct
   let to_string (loc:Location.t) : string =
-    let line = Location.read_line loc in
-    let (left, mid, right) =
-      line
-      |> Interval.split (Location.interval loc)
-    in
+    let (left, mid, right) = Location.split loc in
     let lineno = Location.line loc |> Index.to_base1 in
     let prefix = (lineno |> string_of_int) ^ " | " |> make_bold ~color:[] in
     Printf.sprintf "%s%s%s%s" prefix left (make_bold mid) right
 
-
   let pair_to_string (l1:Location.t) (l2:Location.t) : string =
     if l1.filename = l2.filename && l1.line = l2.line then (
-      let line = Location.read_line l1 in
-      let (left, mid, right) =
-        line
-        |> Interval.split (Location.interval l1)
-      in
+      let (left, mid, right) = Location.split l1 in
       let lineno = Location.line l1 |> Index.to_base1 in
       let prefix = (lineno |> string_of_int) ^ " | " |> make_bold ~color:[] in
       let amount = Interval.finish l1.interval |> Index.to_base0 in
       let i = Interval.translate (-amount) (Location.interval l2) in
-      let (r1, r2, r3) = right |> Interval.split i in
+      let (r1, r2, r3) = Slice.from_interval i |> Slice.split right in
       [prefix; left; make_bold mid; r1; make_bold r2; r3]
       |> String.concat ""
     ) else (
