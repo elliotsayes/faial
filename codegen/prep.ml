ad15693bf6c10ec86541b936c3d755a4b2974a29
udaya <u.sathiyamoorthy001@umb.edu>
2023-01-17 09:57:49 -0500

    Removed protocol slicing code

--- codegen/prep.ml
+++ codegen/prep.ml
@@ -89,31 +89,6 @@ let set_block_dim (thread_count : Vec3.t) (k : prog kernel) : prog kernel =
   in
   {k with kernel_code = code}
 
-(* Convert a shared access to a protocol instruction *)
-let rec shared_access_to_inst : Shared_access.t -> inst = function
-  | Loop (r, acc) -> Loop (r, [shared_access_to_inst acc])
-  | Cond (b, acc) -> Cond (b, [shared_access_to_inst acc])
-  (* Assume the access is a read *)
-  | Index a -> Acc (a.shared_array, {index=[a.index]; mode=Rd})
-
-(* Slice a protocol into independently-analyzable shared accesses *)
-let slice_protocol (thread_count : Vec3.t) (k : prog kernel) : prog kernel =
-  let code = Shared_access.from_kernel thread_count k
-             |> Seq.map shared_access_to_inst
-             |> List.of_seq
-  in
-  {k with kernel_code = code}
-
-(* Flatten multi-dimensional arrays into 1D products of their dimensions *)
-let flatten_multi_dim (k : prog kernel) : prog kernel =
-  let flatten_array (arr : Memory.t) : Memory.t =
-    match arr.size with
-    | [] -> arr
-    | size -> {arr with size = [List.fold_left ( * ) 1 size]}
-  in
-  let arrays = VarMap.map flatten_array k.kernel_arrays in
-  {k with kernel_arrays = arrays}
-
 (* Prepare the kernel for serialization *)
 let prepare_kernel
     (racuda : bool)
