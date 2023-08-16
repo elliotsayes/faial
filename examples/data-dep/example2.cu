2c8e3540c0363629f6bdbfe1a08b292d24326a95
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2023-08-16 10:34:26 -0400

    Changed the example slightly so that there is a data-race.

--- examples/data-dep/example2.cu
+++ examples/data-dep/example2.cu
@@ -3,6 +3,5 @@ __global__
 void saxpy(int n, float a, float *x, float *y)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (x[i]) y[i + 1] = a*x[i];
-  y[i] = a*x[i];
+  if (y[i]) x[i + 1] = a*x[i];
 }
