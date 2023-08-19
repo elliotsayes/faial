174356aa84f1a6f8ffe84248b6f04c8e85491194
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2023-08-19 02:50:31 -0400

    Fixed test script.

--- examples/data-dep/run.py
+++ examples/data-dep/run.py
@@ -1,14 +1,16 @@
 import pathlib
 import subprocess
-files = pathlib.Path(".").glob("*.cu")
+base_dir = pathlib.Path(__file__).parent
+exe = base_dir.parent.parent / "data-dep"
+files = base_dir.glob("*.cu")
 def header(f):
     with f.open() as fp:
         return fp.readline()[len("//"):].strip()
 
 for x in sorted(files):
     expected = header(x)
-    given = subprocess.check_output(["data-dep", x]).strip().decode()
+    given = subprocess.check_output([exe, x]).strip().decode()
     if expected != given:
         print(f"{x}: expected={repr(expected)} given={repr(given)}")
     else:
-        print("Checked", x)
+        print(exe, x)
