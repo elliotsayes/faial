67f94d074e669531c95a7071474eab75a31b1ead
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-04-18 17:07:36 -0400

    Test both bounds.

--- examples/drf/drf-loop1.cu
+++ examples/drf/drf-loop1.cu
@@ -1,9 +1,11 @@
 __global__
 void saxpy(int n, float a, float *x, float *y)
 {
+  __requires (n > 0);
   int i = blockIdx.x*blockDim.x + threadIdx.x;
   float f = 0.0f;
-  for (int j = 0; j < n; j++) {
+  y[0] = 0;
+  for (int j = 1; j < n; j++) {
     f += y[j];
     // y[0] ... y[n - 1]
   }
