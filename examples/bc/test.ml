b5f5ba1b05aa2fac5b4e13a74e9d673b20a5ad9c
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-06-14 12:55:35 -0400

    Fixed comment.

--- examples/bc/test.ml
+++ examples/bc/test.ml
@@ -51,8 +51,9 @@ let tests = [
   "loops-1-tid-ub.cu", ["--blockDim=32"], "31";
   (* tid in the upper bound *)
   "loops-1-tid-ub-var.cu", ["--blockDim=32"], "Σ_{i | 0 ≤ i ≤ ((31 + n) - 1)} 1";
-  (* loop range is made uniform and variable used in index *)
+  (* loop range is made uniform and variable used in index (variable is thread local) *)
   "loops-1-tid-var-1.cu", [], "310";
+  (* loop range is made uniform and variable used in index (variable is thread global) *)
   "loops-1-tid-var-2.cu", [], "31744";
   (* a thread-local unknown variable being multiplied by tid is 31 *)
   "unkn-tid.cu", [], "31";
