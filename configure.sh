09078b0328c1ed9049ce6413f378d18c7f3b9f3a
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-08-11 09:52:11 -0400

    Updated dependencies.

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 if which opam > /dev/null; then
     opam install "$@" \
-        dune=3.15.2 \
+        dune=3.16.0 \
         ounit=2.2.7 \
-        cmdliner=1.2.0 \
+        cmdliner=1.3.0 \
         ANSITerminal=0.8.5 \
         toml=7.1.0 \
         otoml=1.0.5 \
