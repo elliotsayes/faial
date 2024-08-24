27b9f09c45794c552041034851dc21b88e70e2f6
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-08-24 17:54:25 -0400

    Added support for bundling in macOS.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 set -x
+Z3_VERSION=4.13.0
+case $(uname -s) in
+  Linux)
+    JOB=build
+    Z3_ARCH=x64-glibc-2.31
+    ;;
+  Darwin)
+    JOB=build-mac
+    Z3_ARCH=arm64-osx-11.0
+    ;;
+
+esac
+JOB=build
 # c-to-json:
-wget -nv --content-disposition "https://gitlab.com/umb-svl/c-to-json/-/jobs/artifacts/main/raw/build/c-to-json-bin.tar.bz2?job=build" -O c-to-json-bin.tar.bz2 &&
+wget -nv --content-disposition "https://gitlab.com/umb-svl/c-to-json/-/jobs/artifacts/main/raw/build/c-to-json-bin.tar.bz2?job=$JOB" -O c-to-json-bin.tar.bz2 &&
 tar xvf c-to-json-bin.tar.bz2 &&
 rm c-to-json-bin.tar.bz2 &&
 # faial:
@@ -14,8 +27,6 @@ cp \
   bin/ &&
 cp ../scripts/faial-drf ../README.md ../LICENSE  ./ &&
 # download z3
-Z3_VERSION=4.13.0 &&
-Z3_ARCH=x64-glibc-2.31 &&
 mkdir lib/ &&
 wget -nv --content-disposition "https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-${Z3_ARCH}.zip" &&
 unzip z3-${Z3_VERSION}-${Z3_ARCH}.zip &&
