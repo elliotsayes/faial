ec02c9a7b0722b9e6e58a29fbcf4b3e5957724e2
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-08-24 22:43:30 -0400

    Fixing CI script to work on macos.

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -5,14 +5,16 @@ case $(uname -s) in
   Linux)
     JOB=build
     Z3_ARCH=x64-glibc-2.31
+    Z3_DLL=libz3.so
     ;;
   Darwin)
     JOB=build-mac
     Z3_ARCH=arm64-osx-11.0
+    Z3_DLL=libz3.dylib
     ;;
 
 esac
-JOB=build
+
 # c-to-json:
 wget -nv --content-disposition "https://gitlab.com/umb-svl/c-to-json/-/jobs/artifacts/main/raw/build/c-to-json-bin.tar.bz2?job=$JOB" -O c-to-json-bin.tar.bz2 &&
 tar xvf c-to-json-bin.tar.bz2 &&
@@ -30,7 +32,7 @@ cp ../scripts/faial-drf ../README.md ../LICENSE  ./ &&
 mkdir lib/ &&
 wget -nv --content-disposition "https://github.com/Z3Prover/z3/releases/download/z3-${Z3_VERSION}/z3-${Z3_VERSION}-${Z3_ARCH}.zip" &&
 unzip z3-${Z3_VERSION}-${Z3_ARCH}.zip &&
-cp z3-${Z3_VERSION}-${Z3_ARCH}/bin/libz3.so lib/ &&
+cp z3-${Z3_VERSION}-${Z3_ARCH}/bin/${Z3_DLL} lib/ &&
 cp z3-${Z3_VERSION}-${Z3_ARCH}/LICENSE.txt ./LICENSE-z3.txt &&
 rm -rf z3-${Z3_VERSION}-${Z3_ARCH}.zip z3-${Z3_VERSION}-${Z3_ARCH}/ &&
 # display tree & bundle:
