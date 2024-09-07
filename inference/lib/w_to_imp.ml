8aa335553d553af63578376a4791f3dc20ffc94e
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-09-06 22:56:14 -0400

    Translating arrays.

--- inference/lib/w_to_imp.ml
+++ inference/lib/w_to_imp.ml
@@ -1,11 +1,46 @@
 open Protocols
 
+let tr_address_space : W_lang.AddressSpace.t-> Mem_hierarchy.t option =
+  function
+  | WorkGroup -> Some Mem_hierarchy.SharedMemory
+  | Storage ReadWrite
+  | Storage WriteOnly -> Some Mem_hierarchy.GlobalMemory
+  | Storage ReadOnly
+  | Uniform
+  | Handle
+  | PushConstant
+  | Function
+  | Private -> None
+
+let tr_type (ty:W_lang.Type.t) : (int list * string list) option =
+  match ty.inner with
+  | Array {base; size} ->
+    Some (Option.to_list size , [W_lang.Type.to_string base])
+  | _ -> None
+
+let tr_decl (d: W_lang.Decl.t) : (Variable.t * Memory.t) option =
+  let ( let* ) = Option.bind in
+  let* h : Mem_hierarchy.t = tr_address_space d.space in
+  let* (size, data_type) = tr_type d.ty in
+  let name = d.name in
+  Some (Variable.from_name name, Memory.{hierarchy=h; size; data_type})
+
 let globals_to_arrays
-  (_args: W_lang.Decl.t list)
+  (globals: W_lang.Decl.t list)
 :
   Memory.t Variable.Map.t
 =
-  Variable.Map.empty
+  globals
+  |> List.filter_map tr_decl
+  |> Variable.Map.of_list
+
+let rec tr_stmt : W_lang.Statement.t -> Imp.Stmt.t option =
+  function
+  | Block l -> Some (tr_block l)
+  | Return None -> None
+  | _ -> None
+and tr_block (l: W_lang.Statement.t list) : Imp.Stmt.t =
+  Block (List.filter_map tr_stmt l)
 
 let entry_to_kernel
   (globals:W_lang.Decl.t list)
@@ -18,7 +53,7 @@ let entry_to_kernel
     ty = "?";
     arrays = globals_to_arrays globals;
     params = Params.empty;
-    code = Block [];
+    code = tr_block e.function_.body;
     visibility = Global;
   }
 
