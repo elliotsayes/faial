13e7c9135d03c2701e1a7531c5d392d4c4146b8a
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2024-11-15 13:11:55 -0500

    Don't discard statements in init.

--- imp/for.ml
+++ imp/for.ml
@@ -67,18 +67,21 @@ module Infer = struct
   type for_ = t
 
   type t = {
+    pre: Stmt.t;
     name: Variable.t;
     init: Exp.nexp;
     cond: Comparator.t unop;
     inc: Increment.t unop;
   }
 
-  let parse_init: Stmt.t -> (Variable.t * Exp.nexp) option =
+  let parse_init: Stmt.t -> (Variable.t * Exp.nexp * Stmt.t) option =
     function
-    | Decl ({var; init=Some data; _}::_)
-    | Seq (Decl [{var; init=Some data; _}], _)
+    | Decl ({var; init=Some data; _}::l) ->
+      Some (var, data, Decl l)
+    | Seq (Decl [{var; init=Some data; _}], s) ->
+      Some (var, data, s)
     | Assign {var; data; _} ->
-      Some (var, data)
+      Some (var, data, Skip)
     | _ -> None
 
   let parse_cond : Exp.bexp -> (Variable.t * Comparator.t unop) option =
@@ -119,20 +122,15 @@ module Infer = struct
     | _ -> None
 
   let parse (loop: for_) : t option =
-    let* (x1, inc) = parse_inc loop.inc in
-    let* init : Exp.nexp =
+    let* (name, inc) = parse_inc loop.inc in
+    let* (init, pre) : Exp.nexp * Stmt.t =
       match parse_init loop.init with
-      | Some (x2, init) ->
-        if Variable.equal x1 x2 then Some init else None
-      | None -> Some (Var x1)
+      | Some (x2, init, pre) ->
+        if Variable.equal name x2 then Some (init, pre) else None
+      | None -> Some (Var name, Stmt.Skip)
     in
     let* (_, cond) = parse_cond loop.cond in
-    Some {
-      name = x1;
-      init = init;
-      cond = cond;
-      inc = inc;
-    }
+    Some {pre; name; init; cond; inc;}
 
   let infer_bounds : t -> Exp.nexp * Exp.nexp * Range.direction =
     function
@@ -166,27 +164,23 @@ module Infer = struct
     | _ -> None
 
 
-  let to_range (r:t) : Range.t option =
-    let (lb, ub, d) = infer_bounds r in
+  let to_range (r:t) : (Stmt.t * Range.t) option =
+    let (lower_bound, upper_bound, dir) = infer_bounds r in
     let* step = infer_step r in
-    Some Range.{
-      var=r.name;
-      lower_bound=lb;
-      upper_bound=ub;
-      step=step;
-      dir=d;
-      ty=C_type.int;
-    }
+    let dst = Range.{
+      var=r.name; lower_bound; upper_bound; step; dir; ty=C_type.int;
+    } in
+    Some (r.pre, dst)
 end
 
 (* Given a for-loop range, output a protocol Range *)
-let to_range (loop:t) : Range.t option =
+let to_range (loop:t) : (Stmt.t * Range.t) option =
   let* inf = Infer.parse loop in
   Infer.to_range inf
 
 let to_stmt (l:t) (body:Stmt.t) : Stmt.t =
   match to_range l with
-  | Some r -> For (r, body)
+  | Some (s, r) -> Stmt.seq s (For (r, body))
   | None -> Star body
 
 let infer_while (cond:Exp.bexp) (body:Stmt.t) : Stmt.t =
